4a0d527b856f47034d06a8bf44736ca8b263090d
Alberto De Agostini <alberto.deagostini@infinitaslearning.com>
2026-01-11 19:41:16 +0100

    feat: add vitest

---
+++ packages/scanner/src/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { scan } from "./index";
+
+describe("scanner", () => {
+	it("should be defined", () => {
+		expect(scan).toBeDefined();
+	});
+});
